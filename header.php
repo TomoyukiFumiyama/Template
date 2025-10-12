e9ec10b2febf2a7e6a8ff8b5a0902e8effdfd6ad
TOMOYUKI FUMIYAMA <114863292+TomoyukiFumiyama@users.noreply.github.com>
2025-10-12 12:37:47 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,14 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 <!DOCTYPE html>
-<html>
+<html <?php language_attributes();?>>
 	<head>
-		<meta charset="utf-8" />
+		<meta charset="<?php bloginfo( 'charset' );?>">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
-		<link rel="preconnect" href="https://fonts.googleapis.com" />
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-		<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
 		<?php wp_head(); ?>
 	</head>
 
-	<body class="page">
+	<body <?php body_class();?>>
+<?php wp_body_open();?>
 		<header class="global-header-wrapper">
 			<div class="global-header-container">
 				<div class="global-header">
