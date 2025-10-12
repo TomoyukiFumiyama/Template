0731538fd917dbc4610b7a7dca39b9a05d53ead7
TOMOYUKI FUMIYAMA <114863292+TomoyukiFumiyama@users.noreply.github.com>
2025-10-12 12:49:11 +0900

    Create search.php

---
+++ search.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying search results pages
+ *
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php if ( have_posts() ) :?>
+
+			<header class="page-header">
+				<h1 class="page-title">
+					<?php
+					printf( esc_html__( 'Search Results for: %s', 'mytheme' ), '<span>'. get_search_query(). '</span>' );
+					?>
+				</h1>
+			</header><?php
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+			get_template_part( 'template-parts/content', 'none' );
+		endif;
+		?>
+
+	</main><?php
+get_sidebar();
+get_footer();
